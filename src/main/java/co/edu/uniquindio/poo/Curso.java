324332b4bc4b43a0f3b1fa7d2093b1b9ee463274
Sebastian Román Sánchez <sebastian.romans@uqvirtual.edu.co>
2024-04-14 19:20:09 -0500

    problema 3 finalizado

--- src/main/java/co/edu/uniquindio/poo/Curso.java
+++ src/main/java/co/edu/uniquindio/poo/Curso.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 import java.util.function.Predicate;
 
 /**
- * Clase para manejar la información de un curso
+ * Clase para manejar la información de un curso.
  * @author Área de programación UQ
  * @since 2024-01
  * 
